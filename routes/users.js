7f66a0e11f7ce042eaf1bac34da8a69fc02bd4fd
fabio4520 <fabioleofc@gmail.com>
2023-04-09 18:18:39 -0500

    passport jwt

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var User = require('../models/users');
 var router = express.Router();
 var passport = require('passport');
+var authenticate = require('../authenticate');
 
 router.use(bodyParser.json());
 
@@ -29,9 +30,11 @@ router.post('/signup', function (req, res, next) {
 });
 
 router.post('/login', passport.authenticate('local'), function (req, res) { 
+  
+  var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, status: 'You are successfully logged in!'});
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });  
 
 router.get('/logout', (req, res) => {
