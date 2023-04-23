26bef5a059909e1cae550ca94eaa630f8a04ce40
fabio4520 <fabioleofc@gmail.com>
2023-04-23 16:27:48 -0500

    task week 3

--- authenticate.js
+++ authenticate.js
@@ -23,7 +23,7 @@ opts.secretOrKey = config.secretKey;
 
 exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
   console.log("JWT payload: ", jwt_payload);
-  User.findOne({_id: jwt_payload._id}, (err, user) => {
+  User.findOne({ _id: jwt_payload._id }, (err, user) => {
     if (err) {
       return done(err, false);
     }
@@ -37,4 +37,16 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => 
 }
 ));
 
-exports.verifyUser = passport.authenticate('jwt', {session: false});
+exports.verifyUser = passport.authenticate('jwt', { session: false });
+
+exports.verifyAdmin = function (req, res, next) {
+  if (req.user.admin) {
+    return next();
+  }
+  else {
+    var err = new Error('You are not authorized to perform this operation!');
+    err.status = 403;
+    return next(err);
+  }
+}
+
