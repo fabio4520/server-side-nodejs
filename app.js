37bb4d4190e9e34e6901954906b11ab54c9245ba
fabio4520 <fabioleofc@gmail.com>
2023-05-06 01:40:08 -0500

    file upload

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var usersRouter = require('./routes/users');
 let dishRouter = require('./routes/dishRouter');
 let promoRouter = require('./routes/promoRouter');
 let leaderRouter = require('./routes/leaderRouter');
+let uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -79,6 +80,7 @@ app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
